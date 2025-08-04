7a06ad6fcfa2b6773190ec4db4cab4a8a9b9b0f2
Jawad Bin Hamid <jawadbinhamid@gmail.com>
2025-08-04 08:43:30 -0700

    Members List page Implementation

--- gymify-be/server.js
+++ gymify-be/server.js
@@ -1,8 +1,10 @@
 const app = require('./app');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
+app.use(cors()); // or configure as needed
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
